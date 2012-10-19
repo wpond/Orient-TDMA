a03bdcecc4db12de7885d08142c05d692c6d6375
wpond <w.pond@sms.ed.ac.uk>
2012-10-19 14:35:11 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,174 @@
+/*
+
+Main file for SLIP D embedded software
+
+*/
+
+/* includes */
+#include "efm32.h"
+
+#include "efm32_chip.h"
+#include "efm32_rtc.h"
+#include "efm32_gpio.h"
+#include "efm32_cmu.h"
+
+#include "limits.h"
+
+#include "led.h"
+
+/* variables */
+
+
+/* prototypes */
+void InitClocks();
+void HandleInterrupt();
+void startupLEDs();
+void updateLEDs(LED color);
+void wait(uint32_t ms);
+
+/* functions */
+// messy interrupt handler
+void GPIO_EVEN_IRQHandler(void) 
+{
+	HandleInterrupt();
+}
+void GPIO_ODD_IRQHandler(void)
+{
+	HandleInterrupt();
+}
+
+void HandleInterrupt()
+{
+	
+}
+
+void wait(uint32_t ms)
+{
+	
+	uint32_t time, 
+		clockFreq = CMU_ClockFreqGet(cmuClock_RTC);
+	
+	while (ms > 0)
+	{
+		
+		time = RTC_CounterGet();
+		
+		if (UINT_MAX - time < ((double)ms / 1000.0) * clockFreq)
+		{
+			ms -= (uint32_t)(1000.0 * ((UINT32_MAX - time) / (double)clockFreq));
+			while (RTC_CounterGet() > time);
+		}
+		else
+		{
+			while (RTC_CounterGet() < time + ((double)ms / 1000.0) * clockFreq);
+			break;
+		}
+		
+	}
+	
+}
+
+void updateLEDs(LED led)
+{
+	
+	LED_Off(RED);
+	LED_Off(BLUE);
+	LED_Off(GREEN);
+	
+	LED_On(led);
+	
+}
+
+void startupLEDs()
+{
+	
+	LED_Off(RED);
+	LED_Off(BLUE);
+	LED_Off(GREEN);
+	
+	uint32_t time = RTC_CounterGet();
+	while (RTC_CounterGet() < time + 32768);
+	
+	LED_On(RED);
+	LED_On(BLUE);
+	LED_On(GREEN);
+	
+	time = RTC_CounterGet();
+	while (RTC_CounterGet() < time + 32768);
+	
+	LED_Off(RED);
+	LED_Off(BLUE);
+	LED_Off(GREEN);
+	
+	time = RTC_CounterGet();
+	while (RTC_CounterGet() < time + 32768);
+	
+}
+
+void InitClocks()
+{
+	/* Starting LFXO and waiting until it is stable */
+	CMU_OscillatorEnable(cmuOsc_LFXO, true, true);
+
+	// starting HFXO, wait till stable
+	CMU_OscillatorEnable(cmuOsc_HFXO, true, true);
+
+	// route HFXO to CPU
+	CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFXO);
+
+	/* Routing the LFXO clock to the RTC */
+	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
+	CMU_ClockSelectSet(cmuClock_LFB, cmuSelect_LFXO);
+
+	// disabling the RCs
+	CMU_ClockEnable(cmuSelect_HFRCO, false);
+	CMU_ClockEnable(cmuSelect_LFRCO, false);
+
+	/* Enabling clock to the interface of the low energy modules */
+	CMU_ClockEnable(cmuClock_CORE, true);
+	CMU_ClockEnable(cmuClock_CORELE, true);
+
+	// enable clock to hf perfs
+	CMU_ClockEnable(cmuClock_HFPER, true);
+
+	// enable clock to GPIO
+	CMU_ClockEnable(cmuClock_GPIO, true);
+
+	// enable clock to RTC
+	CMU_ClockEnable(cmuClock_RTC, true);
+	RTC_Enable(true);
+
+}
+
+int main()
+{
+	
+	// Chip errata
+	CHIP_Init();
+	
+	// ensure core frequency has been updated
+	SystemCoreClockUpdate();
+	
+	// start clocks
+	InitClocks();
+	
+	// init LEDs
+	LED_Init();
+	
+	// show startup LEDs
+	startupLEDs();	
+	
+	uint8_t color = 0;
+	
+	while (1)
+	{
+		
+		updateLEDs(color);
+		
+		wait(1000);
+		
+		color = (++color) % 3;
+		
+	}
+	
+}
